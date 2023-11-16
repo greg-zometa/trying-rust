c1fda85c37e5b4f31b7c4078ae182598bfd47402
Greg Zometa <greg.so.meta@gmail.com>
2023-11-15 21:38:34 -0800

    don't start a fire kids 🧃

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-fn add(num_one: i32, num_two: i32) -> i32 {
-    return num_one + num_two;
-}
-
-fn display_res(res: i32) {
-    println!("{:?}", res);
-}
-
 fn main() {
-    let num_one = 1;
-    let num_two = 2;
-
-    let sum = add(num_one, num_two);
-    display_res(sum);
+    let match_box = "matches";
+    match match_box {
+        "matches" => println!("Box full of matches"),
+        "few matches" => println!("Theres some matches"),
+        _ => println!("Nota match box")
+    }
 }
