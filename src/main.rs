1afa0e1e6cf92e98b572dbbe7bc0167c077ab26e
Greg Zometa <greg.so.meta@gmail.com>
2023-11-15 22:10:30 -0800

    ran some code 🎮

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-fn main() {
-    let match_box = "matches";
-    match match_box {
-        "matches" => println!("Box full of matches"),
-        "few matches" => println!("Theres some matches"),
-        _ => println!("Nota match box")
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+fn which_way(go: Direction) -> &'static str {
+    match go {
+        Direction::Up => "⬆️",
+        Direction::Down => "⬇️",
+        Direction::Left => "⬅️",
+        Direction::Right => "➡️"
     }
+}
+
+fn main() {
+    let go_up = which_way(Direction::Up);
+    let go_left = which_way(Direction::Left);
+    let go_down = which_way(Direction::Down);
+    let go_right = which_way(Direction::Right);
+
+    println!("{go_up} {go_up} {go_down} {go_down} {go_left} {go_right} {go_left} {go_right} 🅱️ 🅰️");
 }
