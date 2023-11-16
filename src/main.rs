2625856d7a7f82868c443f60ad8df6d9c5b36014
Greg Zometa <greg.so.meta@gmail.com>
2023-11-15 20:39:17 -0800

    basic art 🧮

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-fn say_my_name(first_name: &str, last_name: &str) -> () {
-    println!("Hello {first_name} {last_name}!");
+fn add(num_one: i32, num_two: i32) -> i32 {
+    return num_one + num_two;
+}
+
+fn display_res(res: i32) {
+    println!("{:?}", res);
 }
 
 fn main() {
-    let first_name = "Matt";
-    let last_name = "Damon";
-    say_my_name(first_name, last_name)
+    let num_one = 1;
+    let num_two = 2;
+
+    let sum = add(num_one, num_two);
+    display_res(sum);
 }
