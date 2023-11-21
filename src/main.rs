936b53f7a42d4aab071d12696d472f4cbab92e79
Greg Zometa <greg.so.meta@gmail.com>
2023-11-20 22:04:47 -0800

    strings 🧵

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
-fn main() {
-    let numbers = vec![10, 20, 30, 40];
+struct Person {
+    age: i32,
+    name: String,
+    fav_color: String
+}
 
-    for num in &numbers {
-        match num {
-            30 => println!("thirty"),
-            _ => println!("{:?}", num)
+fn print(data: &str) {
+    println!("{:?}", data);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            age: 20,
+            name: String::from("Georgie"),
+            fav_color: String::from("grey")
+        },
+        Person {
+            age: 21,
+            name: String::from("Mikee"),
+            fav_color: String::from("orange")
+        },
+        Person {
+            age: 13,
+            name: String::from("Chris"),
+            fav_color: String::from("black")
+        },
+        Person {
+            age: 8,
+            name: String::from("Bobby"),
+            fav_color: String::from("red")
+        },
+        Person {
+            age: 5,
+            name: String::from("Alan"),
+            fav_color: String::from("blue")
+        },
+        Person {
+            age: 40,
+            name: String::from("Adam"),
+            fav_color: String::from("purple")
+        },
+        Person {
+            age: 30,
+            name: String::from("Kevin"),
+            fav_color: String::from("navy blue")
+        },
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            print(&person.name);
+            print(&person.fav_color)
         }
     }
-
-    println!("The length of numbers is {:?}", numbers.len());
 }
